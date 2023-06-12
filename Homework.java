e0822b2d2f60d7df4f880a1a35e0149f59f6d117
Liasanna <112963336+Liasanna@users.noreply.github.com>
2023-06-12 15:26:48 +0400

    Create Homework.java

---
+++ Homework.java
@@ -0,0 +1,28 @@
+public class ExcClass extends Exception {
+ 
+    private String someString;
+ 
+    public ExcClass (String string) {
+        this.someString = string;
+        System.out.println("Exception ExcClass");
+    }
+ 
+    public void myOwnExceptionMsg() {
+        System.err.println("This is exception message for string: " + someString);
+    }
+}
+ 
+public class TestExc {
+ 
+    public static void main(String[] args) {
+        try {
+            String s = "SomeString";
+            throw new ExcClass(s);
+        } catch (ExcClass ex) {
+            ex.myOwnExceptionMsg();
+        }
+    }
+}
+//Вывод
+Exception ExcClass
+This is exception message for string: SomeString
